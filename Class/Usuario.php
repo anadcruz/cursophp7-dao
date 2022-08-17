8cb8b66b74ec15d69662e93b500cd261d3522818
Ana Cruz <anaclara.dcruz@gmail.com>
2022-08-17 08:52:50 -0300

    métodos de lista

--- Class/Usuario.php
+++ Class/Usuario.php
@@ -58,8 +58,48 @@ if (count($results) > 0){
 	$this->setDtcadastro(new DateTime($row['dtcadastro']));
 
 	}
+}
+
+public static function getList(){
+
+	$sql = new Sql();
+
+	return $Sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin;");
+}
+
+public static function search($login){
+
+	$sql = new Sql();
+
+	return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array( ':SEARCH' => "%".$login."%"));
+}
+
+public fuction login($login, $password){
+
+	$sql = new Sql();
+
+ 	$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD",array(
+	":LOGIN"=>$login,
+	":PASSWORD"=>$password
+
+ ));
+
+if (count($results) > 0){
+
+	$row = $results[0];
+
+	$this->setIdusuario($row['idusuario']);
+	$this->setDessenha($row['dessenha']);
+	$this->setDeslogin($row['deslogin']);
+	$this->setDtcadastro(new DateTime($row['dtcadastro']));
+
+	} else {
+
+		throw new Exception("Login e/ou senha inválidos.");
+	}
 
 }
+
 
 public function __toString(){
 
